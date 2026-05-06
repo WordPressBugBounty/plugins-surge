7ebc08ca3297898c962f23e865537c7f5144960c
Sean Murphy <sean@iamseanmurphy.com>
2026-05-06 17:46:24 +0000

    1.2.1

--- surge/include/common.php
+++ surge/include/common.php
@@ -70,11 +70,15 @@ function config( $key ) {
 		$config = array_merge( $config, $_config );
 	}
 
-	foreach ( $config as $key => $value ) {
-		$const = 'SURGE_' . strtoupper( $key );
+	foreach ( $config as $config_key => $value ) {
+		$const = 'SURGE_' . strtoupper( $config_key );
 		if ( defined( $const ) ) {
-			$config[ $key ] = constant( $const );
+			$config[ $config_key ] = constant( $const );
 		}
+	}
+
+	if ( defined( 'SURGE_CONFIG_CALLBACK' ) && is_callable( SURGE_CONFIG_CALLBACK ) ) {
+		$config = call_user_func( SURGE_CONFIG_CALLBACK, $config );
 	}
 
 	return $config[ $key ];
